5916fcac54a389a1c8db8f2da4afc9aaf68441b6
Raz Carolino <rcrlno97@outlook.com>
2020-03-01 16:33:32 +0800

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
 
 api.listen(3000, () => {
-    console.log('API is running!')
+    console.log('API is up and running!');
 });
 
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello World!');
+api.post('/add', (res, req) => {
+    console.log('Post request received');
 });
+
+// api.get('/', (req, res) => {
+//     console.log(req);
+//     res.send('Hello World!');
+// });
+
+// Middleware sample
+// api.use((req, res, next) => {
+//     console.log('Hello');
+//     next();
+// });
 
