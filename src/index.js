ae4136ce6bafccb65432a555c087307b9843f348
Raz Carolino <rcrlno97@outlook.com>
2020-03-03 10:47:18 +0800

    add function initialization using mysql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const bodyParser = require("body-parser");
 const express = require('express');
+const mysql = require('mysql2');
+
+// connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todo'
+});
+
+// red flag (connection)
+try {
+    connection.connect();
+} catch (e) {
+    console.log('Ops. Connection to MySQL failed.');
+    console.log(e);
+}
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
@@ -11,7 +28,17 @@ api.listen(3000, () => {
 
 api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+
+    connection.query('INSERT INTO tasks (description) VALUES (?)',
+    [req.body.item], (error, results) => {
+        if (error) return res.json({ error: error });
+
+        connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+            if (error) return res.json({ error: error });
+
+            console.log(results);
+        });
+    });
 });
 
 // api.get('/', (req, res) => {
